ec1018051a3fe80a4c7e13776417fda84806c20e
charlierehder <charlesreh1577@gmail.com>
2021-05-26 23:45:11 -0500

    Basic user creation implemented

--- host_a_skier/host_a_skier/settings.py
+++ host_a_skier/host_a_skier/settings.py
@@ -45,6 +45,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hostaskier.apps.HostaskierConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,3 +139,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
