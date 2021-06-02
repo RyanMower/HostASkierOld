ba9af3137139b8cf92cf7544aa386aafa71b2bdf
charlierehder <charlesreh1577@gmail.com>
2021-06-02 17:45:57 -0500

    user can update profile picture, picture now appears next to user posts

--- host_a_skier/users/views.py
+++ host_a_skier/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .forms import UserUpdateForm
+from .forms import ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 def register(request):
@@ -28,4 +30,23 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+
+    # create an instance of a user update form
+    # and a profile update form
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'changes updated successfully')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    # pass both forms into render as a context dictionary
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
